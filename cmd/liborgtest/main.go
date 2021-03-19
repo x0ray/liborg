15657d8ec9e5bee864a0e5941f3cb9c5f77eb56f
x0ray <nzkiwi1g@gmail.com>
2021-03-19 15:57:34 -0400

    added nested structs

--- cmd/liborgtest/main.go
+++ cmd/liborgtest/main.go
@@ -1,6 +1,20 @@
 // Package main -  command liborgtest
 package main
 
+/* Expected Output:
+david@rat:~/go/src/github.com/x0ray/liborg$ liborgtest
+2021/03/19 15:47:40 Package Lib 0.0.9 Dump
+2021/03/19 15:47:40   Name  : Dave  Tag for lib(0): lib.name
+2021/03/19 15:47:40   Name A:   Tag for NestA(0): nesta.name
+2021/03/19 15:47:40   Name B: Dave_C  Tag for NestB(0):
+2021/03/19 15:47:40   Name C: Dave_C  Tag for NestC(0): nestc.name
+2021/03/19 15:47:40 Package Lib 0.0.9 Dump
+2021/03/19 15:47:40   Name  : Dave  Tag for lib(0): lib.name
+2021/03/19 15:47:40   Name A: Fred_A  Tag for NestA(0): nesta.name
+2021/03/19 15:47:40   Name B: Fred_B  Tag for NestB(0):
+2021/03/19 15:47:40   Name C: Fred_B  Tag for NestC(0): nestc.name
+*/
+
 import (
 	o "github.com/x0ray/liborg"
 )
@@ -9,5 +23,5 @@ func main() {
 	d := o.NewLiborg("Dave")
 	d.Dump()
 	d.SetName("Fred")
-	d.Dump() 
+	d.Dump()
 }
--- liborg.go
+++ liborg.go
@@ -1,32 +1,75 @@
 // Package liborg go library organization demo
+// This package only serves as a demonstration of:
+//   An object library
+//    - Constant name LibVer can be seen in the importing package
+//    - Types  can be seen in the importing package
+//   Nested structure member visibility
+//   Struct member tags
 package liborg
 
 import (
+	"fmt"
 	"log"
+	"reflect"
 )
 
-const LibVer = "0.0.2"
+const LibVer = "0.0.9"
 
-// Lib is a demo type
-type Lib struct {
-	name string
+// NestA is a nested demo type
+type NestA struct {
+	Name string `mytag:"nesta.name"` // dont use blanks around colon :
+}
+
+// NestB is a nested demo type
+type NestB struct {
+	NestC
+}
+
+// NestC is a nested demo type
+type NestC struct {
+	Name string `mytag:"nestc.name"`
+}
+
+// lib is a demo type
+type lib struct {
+	Name string `mytag:"lib.name"`
+	NestA
+	NestB
+}
+
+type Face interface {
+	SetName(string)
 }
 
 // New Loborg creates and initializes a new Lib struct
-func NewLiborg(name string) *Lib {
-	l := new(Lib)
-	l.name = name
+func NewLiborg(name string) *lib {
+	l := new(lib)
+	l.Name = name
+	l.NestB.NestC.Name = name + "_C"
 	return l
 }
 
-
-// SetName alters the name of the Lib struct
-func (l *Lib) SetName(name string) {
-	l.name = name 
+// SetName alters the name of the lib struct
+func (l *lib) SetName(name string) {
+	l.NestA.Name = name + "_A"
+	l.NestB.Name = name + "_B"
 }
 
 // Dump displays the contents of the Lib struct on the log
-func (l *Lib) Dump() {
-	log.Printf("Package Lib %s Dump",LibVer)
-	log.Printf("  Name: %s",l.name)
+func (l *lib) Dump() {
+	// get the tag info for the field of the struct
+	t := func(ifce interface{}, f int) string {
+		var mytag string
+		st := reflect.TypeOf(ifce)
+		n := st.Name()
+		field := st.Field(f)
+		mytag = field.Tag.Get("mytag")
+		return fmt.Sprintf("%s(%d): %s", n, f, mytag)
+	}
+	// print out the lib details
+	log.Printf("Package Lib %s Dump", LibVer)
+	log.Printf("  Name  : %s  Tag for %s", l.Name, t(lib{}, 0))
+	log.Printf("  Name A: %s  Tag for %s", l.NestA.Name, t(NestA{}, 0))
+	log.Printf("  Name B: %s  Tag for %s", l.NestB.Name, t(NestB{}, 0))
+	log.Printf("  Name C: %s  Tag for %s", l.NestB.NestC.Name, t(NestC{}, 0))
 }
